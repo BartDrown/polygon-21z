f7aa19e1f02a2eed28f70021a6397c0c7f833636
Krzysztof Rudnicki <krzysztofrudnicki0@gmail.com>
2021-11-08 15:01:23 +0100

    feat: Add health bar

---
+++ Assets/playerHealth.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerHealth : MonoBehaviour
+{
+    public int maxHealth = 100; // Nie daje const bo nie wiem czy nie bêdziemy max healtha zmieniaæ
+    public int currentHealth;
+
+    public healthBarScript healthBar;
+
+    void setPlayerHealthToValue(int valueToBeSet)
+    {
+        currentHealth = maxHealth;
+        healthBar.setMaxValue(maxHealth);
+    }
+
+
+    void Start()
+    {
+        setPlayerHealthToValue(maxHealth);
+    }
+    
+    void DEBUG_removeHpOnSpace(int damage) // originally to check slider functionality
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(damage);
+        }
+    }
+
+    void Update()
+    {
+        DEBUG_removeHpOnSpace(20);
+    }
+
+    void TakeDamage(int damage)
+    {
+        if (currentHealth - damage < 0)
+        {
+            currentHealth = 0;
+            healthBar.setValue(currentHealth);
+        }
+        else
+        {
+            currentHealth -= damage;
+            healthBar.setValue(currentHealth);
+        }
+    }
+
+
+}
