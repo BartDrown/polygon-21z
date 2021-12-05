0da87a648119177329834f2ac7ec2997891113c4
Pawel <ratalewski@wp.pl>
2021-12-05 17:24:22 +0100

    add animation impact and fiix enemy movement

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -12,6 +12,8 @@ public class Enemy : MonoBehaviour
     [SerializeField] private float moveSpeed;
     private Vector2 movementDirection;
     private Vector2 movementPerSecond;
+    private float distanceToPlayer = 12.0f;
+
 
     [SerializeField] GameObject bullet;
     // [SerializeField] GameObject deathEffect; uncomment and add in unity once we have deathEffect
@@ -27,9 +29,9 @@ public class Enemy : MonoBehaviour
     {
 
         CheckIfTimeToFire();
-
+        float dist = Vector2.Distance(this.transform.position, new Vector2(0, 0));
         //if the changeTime was reached, calculate a new movement vector
-        if (Time.time - latestDirectionChangeTime > directionChangeTime)
+        if (Time.time - latestDirectionChangeTime > directionChangeTime || dist > distanceToPlayer)
         {
             latestDirectionChangeTime = Time.time;
             calcuateNewMovementVector();
@@ -66,7 +68,7 @@ public class Enemy : MonoBehaviour
     public void takeDamage(int damage) // public because accessible by BulletScript.cs OnTriggerEnter2D()
     {
         health -= damage;
-        if(health <= 0)
+        if (health <= 0)
         {
             die();
         }
@@ -76,7 +78,10 @@ public class Enemy : MonoBehaviour
     {
         // Instantiate(deathEffect, transform.position, Quaternion.identity);
         // uncomment and add in unity once we have deathEffect
-        Destroy(gameObject);
+        if (gameObject != null)
+        {
+            Destroy(gameObject);
+        }
     }
 
 
